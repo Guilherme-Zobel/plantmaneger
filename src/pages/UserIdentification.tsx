019bab7029fc45528ab15c25ca915792d0df6590
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-11 15:07:37 -0300

    style: estilização da página UserIdentification

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
   View,
   Text,
   TextInput
 } from  'react-native';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function UserIdentification(){
   return (
@@ -26,3 +29,43 @@ export function UserIdentification(){
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 54,
+    alignItems: 'center',
+  },
+  emoji: {
+    fontSize: 44,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 20
+  }
+})
