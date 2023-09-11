1935abfd13d4692045a48081e8cb26ad1be3fbc0
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-11 15:20:35 -0300

    style: Estilização do componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {
   TouchableOpacity,
   Text,
+  StyleSheet
 } from 'react-native';
 
 import fonts from '../styles/fonts';
 import colors from '../styles/colors';
 
 export function Button () {
-  return (
+  return(
     <TouchableOpacity style={styles.container}>
       <Text style={styles.text}>
           Contirmar
@@ -16,3 +17,18 @@ export function Button () {
     </TouchableOpacity>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.green,
+    height: 56,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    fontFamily: fonts.heading
+  }
+})
