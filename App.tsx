a5c2f01e2ec5d591fcc6ea70b89a54452fef33a8
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-19 15:39:10 -0300

    fix: troca expo-app-loading para expo-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,22 +1,34 @@
-import { Welcome } from './src/pages/Welcome';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import Routes from './src/routes';
 
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost'
+import { useEffect } from 'react';
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     Jost_400Regular,
-    Jost_600SemiBold
+    Jost_600SemiBold,
   });
 
-  if(!fontsLoaded)
-  return <AppLoading />
-  
+  useEffect(() => {
+    async function loadApp() {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+    }
+    loadApp();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; 
+  }
+
   return (
-    <Welcome />
+    <Routes />
   );
 }
+
